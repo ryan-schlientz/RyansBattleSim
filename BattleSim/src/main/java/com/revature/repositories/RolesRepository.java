0489c82e065f2442e6fbfcfc7efc653926a6cf6a
Patrick Boronski <plboronski@gmail.com>
2020-08-18 16:03:25 -0400

    Removed commented line

--- BattleSim/src/main/java/com/revature/repositories/RolesRepository.java
+++ BattleSim/src/main/java/com/revature/repositories/RolesRepository.java
@@ -8,7 +8,6 @@ public interface RolesRepository {
 	public int addRole(Role r);
 	public List<Role> getAllRoles();
 	public Role getRole(int id);
-//	public Role getRole(name);
 	public void updateRoles(Role change);
 	public void deleteRoles(int id);
 }
