3c7e05d9373c4876600916b648a47e2ecfc1e21a
Eric Jensen <ejensen9923@mail.kvcc.edu>
2020-08-18 17:25:40 -0400

    fixed project for real this time

---
+++ BattleSim/src/main/java/com/revature/repositories/StatsRepository.java
@@ -0,0 +1,14 @@
+package com.revature.repositories;
+
+import java.util.List;
+
+import com.revature.models.Stats;
+
+public interface StatsRepository {
+	public int addStats(Stats stats);
+	public List<Stats> getAllStats();
+	public Stats getStats(int id);
+	public boolean updateStats(Stats stats);
+	public boolean deleteStats(Stats stats);
+	
+}
