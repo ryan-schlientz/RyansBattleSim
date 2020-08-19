a85bb704103c531d40860b829472d7c495706467
Jack McLean <mcleanojack2@gmail.com>
2020-08-19 17:11:20 -0400

    Added basic create and delete pages for character

---
+++ BattleSimNG/src/app/Components/manage-inventory/manage-inventory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-inventory',
+  templateUrl: './manage-inventory.component.html',
+  styleUrls: ['./manage-inventory.component.css']
+})
+export class ManageInventoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
